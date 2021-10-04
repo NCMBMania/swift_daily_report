73e1aedb46c6400eccd3ac7ed7bec7a053e34a64
Atsushi <atsushi@moongift.jp>
2021-10-04 18:32:06 +0900

    Initial Commit

---
+++ daily_report/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  daily_report
+//
+//  Created by Atsushi on 2021/10/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ daily_report/daily_reportApp.swift
@@ -0,0 +1,17 @@
+//
+//  daily_reportApp.swift
+//  daily_report
+//
+//  Created by Atsushi on 2021/10/04.
+//
+
+import SwiftUI
+
+@main
+struct daily_reportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
